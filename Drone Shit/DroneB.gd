537a281fa64a7ebe57dddfb4dca1063152b43586
MadManaPoints <139494084+MadManaPoints@users.noreply.github.com>
2023-07-13 14:16:17 -0400

    Add files via upload

---
+++ Drone Shit/DroneB.gd
@@ -0,0 +1,62 @@
+extends CharacterBody2D
+
+@export var bullet: PackedScene
+@onready var spawn_point: Marker2D = $SpawnPoint
+
+var start
+var top = 5
+var bottom = 5
+var left = 100
+var right = 50
+var home = true
+
+var chase = false
+var player
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	velocity = Vector2(.16, .07)
+	start = get_global_position()
+	print(start)
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	chase_after_player(delta)
+	
+func chase_after_player(delta):
+	if chase == true:
+		player = get_node("../../Player/Player")
+		position.y = lerp(position.y, player.position.y - 50, .9 * delta)
+		position.x = lerp(position.x, player.position.x + 75, .6 * delta)
+		home = false
+		
+		if Input.is_action_just_pressed("ui_focus_next"):
+			shoot()
+	else:
+		if home == false:
+			position = lerp(position, start, .7 * delta)
+		
+	if chase == false and position.x < start.x + 5 and position.x > start.x - 5 and position.y < start.y + 2 and position.y > start.y - 2:
+		home = true
+			
+	if home == true:
+		position += velocity
+		if position.y > start.y + bottom or position.y < start.y - top:
+			velocity.y *= -1
+		if position.x > start.x + right or position.x < start.x - left: 
+			velocity.x *= -1
+			
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
+
+func _on_timer_timeout():
+	pass
+	
+func shoot():
+	var inst: Bullet = bullet.instantiate()
+	owner.add_child(inst)
+	inst.transform = spawn_point.global_transform
