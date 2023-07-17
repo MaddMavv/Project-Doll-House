8fd87f176e7eac19c226461565935f31112c518f
MadManaPoints <mannyvivas21@gmail.com>
2023-07-17 13:33:39 -0400

    Drone Projectile Fix

--- First Prototype copy 2/bullet.gd
+++ First Prototype copy 2/bullet.gd
@@ -1,15 +1,27 @@
 extends Area2D
 
 class_name Bullet
-var player
 
-const speed = 100
+# @export makes it so anyone can change the speed in the editor.
+# Also makes the variable available to other scripts.
+@export var speed := 400.0
 
-# Called when the node enters the scene tree for the first time.
+# Initializes a direction variable so when this bullet is instantiated, 
+# there's a direction var that can be set.
+var direction := Vector2.ZERO
+
+# This uses Godot's "Groups" which can be set in the inspector for each node.
+# I created a "Player" Group and added the Player node to it.
+# When this script loads, the var player gets a reference to the Player
+# so we can know their position.
+@onready var player := get_tree().get_first_node_in_group("Player")
+
 func _ready():
-	pass
-	#player = get_node("../../../Player/Player")
-	#print(player.position.x)	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	# This is just so the bullet sprite is rotated to face the player.
+	# This fuction needs a Vector2 to know where to rotate the node towards.
+	look_at(position + direction)
+
+
 func _physics_process(delta):
-	pass
+	# Moves the bullet over time towards a direction with a certain speed.
+	position += direction * speed * delta
