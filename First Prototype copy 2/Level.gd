5aeef45889b5604da5f53b8a71e0d721d7ef8e95
T3CNEO <neoshon421@gmail.com>
2023-07-16 23:52:41 -0400

    Added Trains

--- First Prototype copy 2/Level.gd
+++ First Prototype copy 2/Level.gd
@@ -3,9 +3,12 @@ extends Node2D
 
 var knock = preload("res://knockback_projectile.tscn")
 var stun = preload("res://stun_projectile.tscn")
+var train = preload("res://train.tscn")
 var player
 var pain = false
 var howLong = 0;
+var whichTrain
+var trainWait = 0
 
 func attack():
 		var knockTemp = knock.instantiate()
@@ -32,3 +35,15 @@ func _physics_process(delta):
 			stunAttack();
 		pain = false;
 		howLong = 0;
+	trainWait += 1;
+	if trainWait == 100:
+		whichTrain = randi_range(1,3)
+		var trainTemp = train.instantiate()
+		add_child(trainTemp)
+		if whichTrain == 1:
+			trainTemp.position = Vector2(6807, -244)
+		if whichTrain == 2:
+			trainTemp.position = Vector2(6807, 325)
+		if whichTrain == 3:
+			trainTemp.position = Vector2(6807, 894)
+		trainWait = 0
--- First Prototype copy 2/bar_b.gd
+++ First Prototype copy 2/bar_b.gd
@@ -13,31 +13,36 @@ var jumpCount = maxjumpCount
 
 @onready var anim = get_node("AnimationPlayer")
 
+var veloReal = 0;
+var veloOw = 0;
+
+var veloRealY = 0;
+var veloOwY = 0;
+
 
 func _physics_process(delta):
 	# Add the gravity.
 	if is_on_floor():
 		jumpCount = 0
+		veloRealY = 0
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		veloRealY += gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		if Input.is_action_pressed("down"):
 			set_collision_mask_value(2, false)
 		else:
-			velocity.y = JUMP_VELOCITY
+			veloRealY = JUMP_VELOCITY
 			jumpCount = maxjumpCount
 		#anim.play("Jump")
 	# Handle Double and Triple Jump
 	if jumpCount > 0 and Input.is_action_just_pressed("ui_accept"):
-		velocity.y = JUMP_VELOCITY
+		veloRealY = JUMP_VELOCITY
 		#anim.play("Jump")
 		jumpCount -= 1
 		$jumpsound.play()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction == -1:
 			self.rotation_degrees = 180
@@ -48,17 +53,23 @@ func _physics_process(delta):
 			self.scale.y = 1
 			flip = false
 	
+	velocity.x = veloReal+veloOw
+	velocity.y = veloRealY+veloOwY
+	
+	veloOw = move_toward(veloOw, 0, 50)
+	veloOwY = move_toward(veloOwY, 0, 50)
+	
 	if direction:
-		velocity.x = direction * SPEED
-		if velocity.y == 0:
+		veloReal = direction * SPEED
+		if veloRealY == 0:
 			if meleeing == false:
 				anim.play("Run")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)		
-		if velocity.y == 0:
+		veloReal = move_toward(veloReal, 0, SPEED)
+		if veloRealY == 0:
 			if meleeing == false:
 				anim.play("Idle")
-	if velocity.y > 0:
+	if veloRealY > 0:
 			if meleeing == false:
 				pass
 				#anim.play("Fall")
@@ -85,7 +96,12 @@ func shout():
 		await anim.animation_finished
 		meleeing = false;
 		print("heck")
-
+		
+func TRAIN():
+	Game.playerHP -= 1;
+	veloOw = -3000;
+	veloOwY = -1500;
+	
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Enemies"):
