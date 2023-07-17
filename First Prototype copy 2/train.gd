dee69d9c9a867c6fa1f84d847333355fc86a0c8b
T3CNEO <neoshon421@gmail.com>
2023-07-17 14:32:42 -0400

    warns you of trains now

--- First Prototype copy 2/train.gd
+++ First Prototype copy 2/train.gd
@@ -7,8 +7,7 @@ var pog = 0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
