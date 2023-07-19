0d8349eaa71b9e7438f95a4c1fded5eb1c95e48a
MadManaPoints <mannyvivas21@gmail.com>
2023-07-19 17:06:28 -0400

    Red Drone added

--- First Prototype copy 2/test_move.gd
+++ First Prototype copy 2/test_move.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 var targetX
 var radius = 200
-var move = 0.01
+var move = Vector2(0, 0)
+var velocity = Vector2(1, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,7 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	move += 0.01
-	targetX = cos(move) * radius + 600
+	move.x += velocity.x * delta
+	targetX = cos(move.x) * radius + 600
 	position.x = targetX
 	print(position.x)
