41cb32b6a7262251baf1b38be58f92f23182cfd7
Maverick _Unit #100011000 <parisshakan@gmail.com>
2023-07-31 18:28:18 -0400

    increased player speed and made gravity less floaty

--- bar_b.gd
+++ bar_b.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 900.0
-const JUMP_VELOCITY = -1100.0
+const SPEED = 1200.0
+const JUMP_VELOCITY = -1300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -48,7 +48,7 @@ func _physics_process(delta):
 		if Input.is_action_just_released("move down"):
 			veloRealY = JUMP_VELOCITY
 			jumpCount = -1
-	var direction = Input.get_axis("move left ", "move right")
+	var direction = Input.get_axis("move left", "move right")
 	if direction < 0:
 			self.rotation_degrees = 180
 			self.scale.y = -1.4
