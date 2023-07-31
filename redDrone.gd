423d8001f4bb72d99a644c183f86e57a40c24443
MadManaPoints <mannyvivas21@gmail.com>
2023-07-31 15:43:03 -0400

    Revert "Merge branch 'main' of https://github.com/MaddMavv/Project-Doll-House"
    This reverts commit 63d867892ebf3c410c4f94f9b3a8c53e7bd8fd37, reversing
    changes made to 37be85247e359791b505ada0267f7de63b9e644a.

--- redDrone.gd
+++ redDrone.gd
@@ -52,13 +52,12 @@ func move(delta):
 		home = false
 		#falls to the ground
 		move_and_collide(fall * delta)
-	elif home == false and position.y > start.y:
+
+	elif stun == false and home == false:
 		position.y -= 225 * delta
 	
-	if stun == false and position.y < start.y and home == false:
-		position.y = start.y
-		home = true 
-		v_move = Vector2(0.00, 0.00)
+	if stun == false and position.y < start.y:
+		home = true
 	
 func shoot_at_player():
 	#shoot function (below) calls bullet using this node
