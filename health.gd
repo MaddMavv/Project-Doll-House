8136cc22172ac5b4740476312ddc2d5a47e9f77f
IPurpleGhostI <139383948+IPurpleGhostI@users.noreply.github.com>
2023-08-02 17:43:16 -0400

    Update health.gd

--- health.gd
+++ health.gd
@@ -2,15 +2,15 @@ extends AnimatedSprite2D
 
 
 
-#func _ready():
-#	play("full")
-#
-#
-#func _process(delta):
-#	if Game.playerHP == 3:
-#		play("ow1")
-#	if Game.playerHP == 2:
-#		play("ow2")
-#	if Game.playerHP == 1:
-#		play("ow3")
+func _ready():
+	play("full")
+
+
+func _process(delta):
+	if Game.playerHP == 3:
+		play("ow1")
+	if Game.playerHP == 2:
+		play("ow2")
+	if Game.playerHP == 1:
+		play("ow3")
 	
