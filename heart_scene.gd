c522507f00020a9db1a43fd33834ac7979860a4e
IPurpleGhostI <139383948+IPurpleGhostI@users.noreply.github.com>
2023-08-02 18:56:03 -0400

    Wooo

--- heart_scene.gd
+++ heart_scene.gd
@@ -5,9 +5,21 @@ var is_left_broken : bool = false
 var is_right_broken : bool = false
 
 func _ready():
-#	break_pieces($BottomHeart)
+#    break_pieces($BottomHeart)
 	pass
 
+
+func _process(delta):
+	if Game.playerHP == 3 and !is_right_broken:
+		
+		is_right_broken = true
+	elif Game.playerHP == 2 and !is_left_broken:
+		
+		is_left_broken = true
+	elif Game.playerHP == 1 and !is_bottom_broken:
+		break_pieces($BottomHeart)
+		is_bottom_broken = true
+		
 
 func break_pieces(parts : Node):
 	for i in parts.get_children():
