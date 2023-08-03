573d83a720cf250f86745fb37f81059234b3c7ae
IPurpleGhostI <139383948+IPurpleGhostI@users.noreply.github.com>
2023-08-03 09:42:53 -0400

    now its goods

--- framestuff.gd
+++ framestuff.gd
@@ -1,4 +1,4 @@
-extends SpriteFrames
+#extends SpriteFrames
 
 class_name SpriteFrameResource
 
@@ -13,21 +13,21 @@ var imageSix =load("res://Deconstructed Heart/Full Heart.png")
 
 func getFrames() -> SpriteFrames:
 	
-	self.add_animation(name)
-	self.add_frame(name, imageOne)
-	self.add_frame(name, imageTwo)
-	self.add_frame(name, imageThree)
-	self.add_frame(name, imageFour)
-	self.add_frame(name, imageFive)
-	self.add_frame(name, imageSix)
+#	self.add_animation(name)
+#	self.add_frame(name, imageOne)
+#	self.add_frame(name, imageTwo)
+#	self.add_frame(name, imageThree)
+#	self.add_frame(name, imageFour)
+#	self.add_frame(name, imageFive)
+#	self.add_frame(name, imageSix)
 	
-	self.set_animation_loop(name,true)
+#	self.set_animation_loop(name,true)
 	
-	self.set_animation_loop(name,0.6)
-	self.set_animation_loop(name,6.0)
-	self.set_animation_loop(name,0.0)
-	self.set_animation_loop(name,0.6)
-	self.set_animation_loop(name,6.0)
-	self.set_animation_loop(name,0.6)
+#	self.set_animation_loop(name,0.6)
+#	self.set_animation_loop(name,6.0)
+#	self.set_animation_loop(name,0.0)
+#	self.set_animation_loop(name,0.6)
+#	self.set_animation_loop(name,6.0)
+#	self.set_animation_loop(name,0.6)
 
-	return self
+#	return self
